a1385249dd4c42850654a341a3fe07bfacb1efe6
elena <kev7570@gmail.com>
2026-01-19 23:13:02 +0300

    Добавлен модуль generators.py и тесты для generators.py

--- main.py
+++ main.py
@@ -1,45 +1,97 @@
-from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date, mask_account_card
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
-if __name__ == "__main__":
-    # Тестирование функции маскировки номера карты
-    card_number = 7000792289606361
-    masked_card = get_mask_card_number(card_number)
-    print(f"Маскировка карты: {masked_card}")  # Ожидаемый результат: 7000 79** **** 6361
+# Пример данных для тестирования
+transactions = [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {
+            "amount": "79114.93",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188"
+    },
+    {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {
+            "amount": "43318.34",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160"
+    },
+    {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {
+            "amount": "56883.54",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229"
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {
+            "amount": "67314.70",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657"
+    }
+]
 
-    # Тестирование функции маскировки счета
-    account_number = 73654108430135874305
-    masked_account = get_mask_account(account_number)
-    print(f"Маскировка счета: {masked_account}")  # Ожидаемый результат: **4305
+# Тестирование функции filter_by_currency
+print("Тест функции filter_by_currency:")
+usd_transactions = filter_by_currency(transactions, "USD")
+for _ in range(3):
+    print(next(usd_transactions))
 
-    # ТЕСТИРОВАНИЕ НОВЫХ ФУНКЦИЙ
-    print("===> Тест новой функции mask_account_card:")
-    test_cases_mask = [
-        ('Visa Platinum 7000792289606361',
-         'Visa Platinum 7000 79** **** 6361'),  # Строка сокращена
-        ('Maestro 7000792289606361',
-         'Maestro 7000 79** **** 6361'),       # Строка сокращена
-        ('Счет 73654108430135874305',
-         'Счет **4305'),                        # Строка сокращена
-    ]
+# Тестирование функции transaction_descriptions
+print("\nТест функции transaction_descriptions:")
+descriptions = transaction_descriptions(transactions)
+for _ in range(len(transactions)):
+    print(next(descriptions))
 
-    for case_input, expected_output in test_cases_mask:
-        actual_result = mask_account_card(case_input)
-        assert actual_result == expected_output, (
-            f"Test failed for '{case_input}'. Expected: {expected_output}, "
-            f"Got: {actual_result}"
-        )
-        print(f"Тест OK: '{case_input}' -> '{actual_result}'")
-
-    # Тестируем преобразование даты
-    test_case_get_date = (
-        "2024-03-11T02:26:18.671407",
-        "11.03.2024"
-    )
-
-    converted_date = get_date(test_case_get_date[0])
-    assert converted_date == test_case_get_date[1], (
-        f"Test failed for '{test_case_get_date[0]}'. Expected: {test_case_get_date[1]}, "
-        f"Got: {converted_date}"
-    )
-    print(f"Тест OK: '{test_case_get_date[0]}' -> '{converted_date}'")
+# Тестирование функции card_number_generator
+print("\nТест функции card_number_generator:")
+for card_number in card_number_generator(1, 5):
+    print(card_number)
