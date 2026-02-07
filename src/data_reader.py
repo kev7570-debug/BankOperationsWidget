954926c8c079ef8dc8650a423c03b0a019981786
elena <kev7570@gmail.com>
2026-02-07 16:37:20 +0300

    Создан новый модуль data_reader, тесты к нему, котоый обрабатывает файлы transactions и transactions_exel. Обновлен README.md

---
+++ src/data_reader.py
@@ -0,0 +1,30 @@
+import csv
+
+import pandas as pd
+
+
+def read_csv(file_path: str) -> list:
+    """
+    Читает финансовый CSV-файл и возвращает список словарей с транзакциями.
+
+    Args:
+        file_path (str): Путь к CSV-файлу.
+
+    Returns:
+        list: Список словарей с транзакциями.
+    """
+    transactions = []
+
+    with open(file_path, 'r', newline='', encoding='utf-8-sig') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            transactions.append(dict(row))
+
+    return transactions
+
+
+# Функция для чтения Excel-файла
+def read_excel(file_path: str) -> list:
+    df = pd.read_excel(file_path)
+    records = df.where(df.notnull(), None).to_dict(orient='records')
+    return [record for record in records if any(record.values())]
